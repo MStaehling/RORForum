42eb85485163e96c9c5464bf1876f4bff4cacde0
Michael Staehling <Michael.staehling@gmail.com>
2016-01-23 16:13:06 -0500

    added html and js

---
+++ app/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+angular
+  .module('32ResultsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial'
+  ])
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+
+    .primaryPalette('blue',
+
+        {
+          'default': '500', // by default use shade 400 from the pink palette for primary intentions
+          'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+          'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+          'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+        }
+      )
+      .accentPalette('orange');
+  })
+
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'spcVm'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  $locationProvider.html5Mode(true).hashPrefix('!');
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,44 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var app = angular.module('32ResultsApp');
+
+app.run(function($log) {
+  $log.debug('App Run');
+});
+
+app.controller('MainCtrl', function($scope, $mdDialog) {
+  var vm = this;
+
+  var action = function(event) {
+    $mdDialog.show(
+      $mdDialog.alert()
+      .title('Secondary Action')
+      .content('Secondary actions can be used for one click actions')
+      .ariaLabel('Secondary click demo')
+      .ok('Neat!')
+      .targetEvent(event)
+    );
+  };
+
+  // download the data into a local object
+  //vm.contact = $firebaseArray(ref);
+
+  vm.submit = function() {
+    sweetAlert('Thank You for signing up.', 'You will now receive future product updates.', 'success');
+        vm.contact.$add({
+          lastName: vm.lastName,
+          firstName: vm.firstName,
+          email: vm.email
+        });
+        vm.lastName = '';
+        vm.firstName = '';
+        vm.email = '';
+      };
+
+
+  vm.cancel = function() {
+    vm.lastName = '';
+    vm.firstName = '';
+    vm.email = '';
+  };
+});
